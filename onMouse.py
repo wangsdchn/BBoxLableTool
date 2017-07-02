30da48f4213449fc93d9c63a71747b3216de848e
wangsdchn <wangsdbit@163.com>
2017-07-02 10:20:19 +0800

    Put more bboxes and can delete one or more

--- onMouse.py
+++ onMouse.py
@@ -9,34 +9,41 @@ class BBoxDrawing:
         self.imgPath=imgPath
         self.img=None
         self.tepImg=None
+        self.bboxes=[]
     def on_mouse(self,event,x,y,flags,param):
         if event==cv2.EVENT_LBUTTONDOWN:
             self.pt0=(x,y)
             self.drawing=False
+            #self.bboxes.clear()
         elif event==cv2.EVENT_LBUTTONUP:
             self.drawing=False
+            self.bboxes.append([self.pt0,self.pt1])
         elif event==cv2.EVENT_RBUTTONDOWN:
             pass
         elif event==cv2.EVENT_RBUTTONUP:
-            pass
+            for (pt0,pt1) in self.bboxes:
+                if x>pt0[0] and x< pt1[0] and y>pt0[1] and y<pt1[1]:
+                    self.bboxes.remove([pt0,pt1])
         elif event==cv2.EVENT_MOUSEMOVE and (flags & cv2.EVENT_FLAG_LBUTTON):
             self.pt1=(x,y)
             self.drawing=True
         elif event==cv2.EVENT_FLAG_RBUTTON:
             pass
     def drawBox(self):
-        #self.img=cv2.imread(self.imgPath)
         self.tepImg=self.img.copy()
-        cv2.rectangle(self.tepImg,self.pt0,self.pt1,(0,0,255),2)
+        for (pt0,pt1) in self.bboxes:
+            cv2.rectangle(self.tepImg,pt0,pt1,(0,0,255),2)
+        if self.drawing:
+            cv2.rectangle(self.tepImg,self.pt0,self.pt1,(0,0,255),2)
     def start(self):
         cv2.namedWindow('lena')
         cv2.setMouseCallback('lena',self.on_mouse)
         self.img=cv2.imread(self.imgPath)
         self.tepImg=self.img.copy()
         while True:
-            if self.drawing:
-                self.drawBox()
+            self.drawBox()
             cv2.imshow('lena',self.tepImg)
+            
             if 27==cv2.waitKey(1)&0xff:
                 break
             "防止误点窗口关闭"
