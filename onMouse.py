b85357341e12311367d111336b43c178aa6cea71
wangsdchn <wangsdbit@163.com>
2017-07-02 16:12:28 +0800

    Wheel control lable

--- onMouse.py
+++ onMouse.py
@@ -10,6 +10,8 @@ class BBoxDrawing:
         self.img=None
         self.tepImg=None
         self.bboxes=[]
+        self.lables=[0,1,2,3]
+        self.iter=0
     def on_mouse(self,event,x,y,flags,param):
         if event==cv2.EVENT_LBUTTONDOWN:
             self.pt0=(x,y)
@@ -34,12 +36,30 @@ class BBoxDrawing:
                 self.drawing=True
         elif event==cv2.EVENT_FLAG_RBUTTON:
             pass
+        elif event==cv2.EVENT_MOUSEWHEEL:
+            print(flags)
+            if flags>0:                
+                if 0==self.iter:
+                    self.iter=3
+                else:
+                    self.iter-=1
+            else:
+                if 3==self.iter:
+                    self.iter=0
+                else:
+                    self.iter+=1
+            print(self.lables[self.iter])
     def drawBox(self):
         self.tepImg=self.img.copy()
         for (pt0,pt1) in self.bboxes:
             cv2.rectangle(self.tepImg,pt0,pt1,(0,0,255),2)
         if self.drawing:
             cv2.rectangle(self.tepImg,self.pt0,self.pt1,(0,0,255),2)
+    def saveimages(self):
+        self.tepImg=self.img.copy()
+        for (pt0,pt1) in self.bboxes:
+            img=self.tepImg[pt0[0]:pt1[0],pt0[1]:pt1[1],:]
+            cv2.imwrite("",img)
     def start(self):
         cv2.namedWindow('lena',cv2.WINDOW_NORMAL)
         cv2.setMouseCallback('lena',self.on_mouse)
@@ -53,6 +73,8 @@ class BBoxDrawing:
             "防止误点窗口关闭"
             if 8==key:
                 self.bboxes.clear()
+            elif 115==key:
+                self.saveimages()
             cv2.setMouseCallback('lena',self.on_mouse)
             
         cv2.destroyAllWindows()
